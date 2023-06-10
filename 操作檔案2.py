d1d1d73d9db14c6b4f15b3800d03ec21ff4ebbec
NTNU_idiot <62146316+banshee0716@users.noreply.github.com>
2023-06-10 15:25:05 +0800

    1.0 and add readme

--- 操作檔案2.py
+++ 操作檔案2.py
@@ -4,11 +4,13 @@ import re
 import tkinter as tk
 from tkinter import filedialog, ttk, messagebox
 
+
 def filter_files_with_content(src, content):
     # 使用正則表達式來過濾檔案
     pattern = re.compile(content)
     files = [f for f in os.listdir(src) if pattern.search(f)]
     return files
+
 
 def move_files_with_content(src, dst, content, progress):
     # 檢查目標資料夾是否存在，不存在則創建
@@ -24,6 +26,29 @@ def move_files_with_content(src, dst, content, progress):
         window.update_idletasks()
 
     messagebox.showinfo("完成", "所有檔案搬移完成！")
+
+
+def copy_files_with_content(src, dst, content, progress):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+
+    files = filter_files_with_content(src, content)
+    total_files = len(files)
+
+    for i, filename in enumerate(files, 1):
+        if os.path.exists(os.path.join(dst, filename)):
+            # 如果檔案或目錄已經存在於目標資料夾，則跳過這個檔案或目錄
+            continue
+        full_path = os.path.join(src, filename)
+        if os.path.isfile(full_path):
+            shutil.copy(full_path, os.path.join(dst, filename))
+        elif os.path.isdir(full_path):
+            shutil.copytree(full_path, os.path.join(dst, filename))
+        progress["value"] = (i / total_files) * 100
+        window.update_idletasks()
+
+    messagebox.showinfo("完成", "所有檔案與目錄複製完成！")
+
 
 def delete_files_with_content(src, content, progress):
     files = filter_files_with_content(src, content)
@@ -47,24 +72,11 @@ def delete_files_with_content(src, content, progress):
     else:
         messagebox.showinfo("取消", "操作已取消！")
 
-def rename_files_with_content(src, old_content, new_content, progress):
-    # 使用正則表達式來過濾檔案
-    pattern = re.compile(old_content)
-    files = [f for f in os.listdir(src) if pattern.search(f)]
-    total_files = len(files)
-
-    for i, filename in enumerate(files, 1):
-        # 進行替換操作，並修改檔案名稱
-        new_filename = re.sub(old_content, new_content, filename)
-        os.rename(os.path.join(src, filename), os.path.join(src, new_filename))
-        progress['value'] = (i / total_files) * 100
-        window.update_idletasks()
-
-    messagebox.showinfo("完成", "所有檔案重新命名完成！")
 
 def browse_directory():
     folder_selected = filedialog.askdirectory()
     return folder_selected
+
 
 def create_gui():
     global window
@@ -74,30 +86,37 @@ def create_gui():
     tk.Label(window, text="源資料夾:").grid(row=0)
     tk.Label(window, text="目標資料夾:").grid(row=1)
     tk.Label(window, text="檔案關鍵字/正則表達式:").grid(row=2)
-    tk.Label(window, text="新的檔案名稱:").grid(row=3)
 
     src_entry = tk.Entry(window)
     dst_entry = tk.Entry(window)
     content_entry = tk.Entry(window)
-    new_content_entry = tk.Entry(window)
 
     src_entry.grid(row=0, column=1)
     dst_entry.grid(row=1, column=1)
     content_entry.grid(row=2, column=1)
-    new_content_entry.grid(row=3, column=1)
 
     src_browse_button = tk.Button(
-        window, text="瀏覽", command=lambda: src_entry.insert(0, browse_directory())
+        window,
+        text="瀏覽",
+        command=lambda: [
+            src_entry.delete(0, tk.END),
+            src_entry.insert(0, browse_directory()),
+        ],
     )
     dst_browse_button = tk.Button(
-        window, text="瀏覽", command=lambda: dst_entry.insert(0, browse_directory())
+        window,
+        text="瀏覽",
+        command=lambda: [
+            dst_entry.delete(0, tk.END),
+            dst_entry.insert(0, browse_directory()),
+        ],
     )
 
     src_browse_button.grid(row=0, column=2)
     dst_browse_button.grid(row=1, column=2)
 
     progress = ttk.Progressbar(window, length=200, mode="determinate")
-    progress.grid(row=6, column=0, columnspan=3, pady=10)
+    progress.grid(row=7, column=0, columnspan=3, pady=10)
 
     move_button = tk.Button(
         window,
@@ -108,6 +127,15 @@ def create_gui():
     )
     move_button.grid(row=4, column=1, pady=10)
 
+    copy_button = tk.Button(
+        window,
+        text="複製檔案",
+        command=lambda: copy_files_with_content(
+            src_entry.get(), dst_entry.get(), content_entry.get(), progress
+        ),
+    )
+    copy_button.grid(row=5, column=1, pady=10)
+
     delete_button = tk.Button(
         window,
         text="刪除檔案",
@@ -115,21 +143,12 @@ def create_gui():
             src_entry.get(), content_entry.get(), progress
         ),
     )
-    delete_button.grid(row=5, column=1, pady=10)
-
-    rename_button = tk.Button(
-        window,
-        text="修改檔案名稱",
-        command=lambda: rename_files_with_content(
-            src_entry.get(), content_entry.get(), new_content_entry.get(), progress
-        ),
-    )
-    rename_button.grid(row=4, column=2, pady=10)
+    delete_button.grid(row=6, column=1, pady=10)
 
     window.mainloop()
 
-create_gui()
 
+create_gui()
 
 """
 在這個新添加的 rename_files_with_content 函數中，我們使用 re.sub() 函數來將檔案名稱中的舊關鍵詞替換為新關鍵詞，然後使用 os.rename() 函數來修改檔案的名稱。我們還在 GUI 中添加了一個新的輸入框來讓用戶輸入新的關鍵詞，以及一個按鈕來觸發這個新的功能。
